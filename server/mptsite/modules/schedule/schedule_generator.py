3228815298272341f439be2db37e3c3039d31e80
Liromanz <teo.tb@mail.ru>
2024-02-22 01:36:58 +0300

    теперь расписание разделены по блокам

--- server/mptsite/modules/schedule/schedule_generator.py
+++ server/mptsite/modules/schedule/schedule_generator.py
@@ -1,6 +1,8 @@
-from ...models import Schedules, Group, DaySchedule
+from django.shortcuts import get_object_or_404
+
+from ...models import Schedules, Group, DaySchedule, DateTemplates
 from .date_helper import DateHelper
-from datetime import date, timedelta
+from datetime import datetime, timedelta
 
 
 class ScheduleGenerator:
@@ -14,44 +16,55 @@ class ScheduleGenerator:
     ###
 
     @staticmethod
-    def generate_by_day(group: Group, selected_date: date = date.today()):
-        schedule = Schedules.objects.filter(date=date.today(), group=group)
-        day = [DaySchedule(date.today(), schedule)]
+    def generate_by_day(group: Group, selected_date: datetime = datetime.today()):
+        schedule = ScheduleGenerator.__get_base_schedule__(group, selected_date).order_by('number_pair')
+        day = [DaySchedule(datetime.today(), schedule)]
         return day
 
     @staticmethod
     def generate_current_week(group: Group):
-        helper = DateHelper(date.today())
+        helper = DateHelper(datetime.today())
 
         dates = helper.get_week_by_date()
-        schedule = Schedules.objects.filter(date__gte=helper.get_first_day_week(), date__lte=helper.get_last_day_week(),
-                                            group=group)
         day = []
         for d in dates:
-            day.append(DaySchedule(d, schedule.filter(date=d).order_by('number_pair')))
+            day.append(DaySchedule(d, ScheduleGenerator.__get_base_schedule__(group, d).order_by('number_pair')))
         return day
 
     @staticmethod
     def generate_next_week(group: Group):
-        helper = DateHelper(date.today() + timedelta(weeks=1))
+        helper = DateHelper(datetime.today() + timedelta(weeks=1))
 
         dates = helper.get_week_by_date()
-        schedule = Schedules.objects.filter(date__gte=helper.get_first_day_week(), date__lte=helper.get_last_day_week(),
-                                            group=group)
+
         day = []
         for d in dates:
-            day.append(DaySchedule(d, schedule.filter(date=d).order_by('number_pair')))
+            day.append(DaySchedule(d, ScheduleGenerator.__get_base_schedule__(group, d).order_by('number_pair')))
         return day
 
     @staticmethod
-    def generate_by_two_dates(group: Group, start_date: date, end_date: date):
+    def generate_by_two_dates(group: Group, start_date: datetime, end_date: datetime):
         helper = DateHelper(start_date)
 
         dates = helper.get_all_days_until(end_date)
-        schedule = Schedules.objects.filter(date__gte=start_date, date__lte=end_date,
-                                            group=group)
+
         day = []
         for d in dates:
-            day.append(DaySchedule(d, schedule.filter(date=d).order_by('number_pair')))
+            day.append(DaySchedule(d, ScheduleGenerator.__get_base_schedule__(group, d)))
 
         return day
+
+    @staticmethod
+    def __get_base_schedule__(group: Group, selected_date: datetime):
+        selected_date = datetime(2024, selected_date.month, selected_date.day)
+
+        blocks = get_object_or_404(DateTemplates, date_from__lte=selected_date, date_end__gte=selected_date)
+        is_even_week = selected_date.isocalendar().week % 2
+        weekday = selected_date.isoweekday()
+
+        rasp = []
+        for para in Schedules.objects.filter(group=group, block_rasp=blocks, date__iso_week_day=weekday):
+            if para.date.isocalendar().week % 2 == is_even_week:
+                rasp.append(para)
+
+        return rasp
--- server/mptsite/views.py
+++ server/mptsite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . import models as m
-import datetime
+from datetime import datetime
 from .modules.schedule.schedule_generator import ScheduleGenerator as gen
 from .modules.Direction_add import Additions
 
@@ -38,8 +38,8 @@ def rasp_page(request):
 
     group = m.Group.objects.get(name='П50-8-22')
 
-    st = datetime.date.fromisoformat('2024-02-05')
-    end = datetime.date.fromisoformat('2024-02-18')
+    st = datetime(2024, 2, 8)
+    end = datetime(2024, 2, 18)
 
     day_rasp = gen.generate_by_two_dates(group, st, end)
     # day_rasp = [i for i in day_rasp if len(i.lessons) > 0]
@@ -58,8 +58,8 @@ def newFile(request):
 def newrasp(request):
     group = m.Group.objects.get(name='П50-8-22')
 
-    st = datetime.date.fromisoformat('2024-02-05')
-    end = datetime.date.fromisoformat('2024-02-18')
+    st = datetime(2024, 5, 1)
+    end = datetime(2024, 5, 15)
 
     day_rasp = gen.generate_by_two_dates(group, st, end)
     #day_rasp = [i for i in day_rasp if len(i.lessons) > 0]
