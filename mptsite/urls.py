cba475d1d75b1091354ebb7cc5b9a6fe5557f7aa
Liromanz <teo.tb@mail.ru>
2024-10-14 00:58:17 +0300

    футер имеет модульность - в него можно через код передавать ссылки

--- mptsite/urls.py
+++ mptsite/urls.py
@@ -4,9 +4,6 @@ from . import views as v
 
 urlpatterns = [
     path('', v.home_page, name='main_page'),
-    path('prepodi/', v.prepods),
-    path('newfile/', v.newFile),
     path('not_auth_student/', v.not_auth_student, name='not_auth_stud'),
-    path('schedule/', v.schedule, name='schedule')
-    # path('lupapupa/<int:num>/', v.prepods)
+    path('schedule/', v.schedule, name='schedule'),
 ]
