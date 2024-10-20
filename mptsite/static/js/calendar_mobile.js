eed01d51d8bd57dd61c9f13e679832cf70c1ef9b
broGideon <turuncev70@gmail.com>
2024-10-21 02:35:27 +0300

    add infinity scroll

--- mptsite/static/js/calendar_mobile.js
+++ mptsite/static/js/calendar_mobile.js
@@ -3,7 +3,9 @@ const calendarMobile = document.getElementById("calendar-mobile");
 const dayMonthFormater = new Intl.DateTimeFormat(locale, { day: 'numeric', month: 'numeric' });
 const weekdayFormater = new Intl.DateTimeFormat(locale, { weekday: 'long' });
 
-let selectIndex = null; // Изначально null, чтобы не было выбранного элемента
+let selectIndex = null;
+let startIndexMobile = null;
+let endIndexMobile = null;
 
 const updateSelect = (selectedIndex) => {
     const allDays = document.querySelectorAll('#calendar-mobile button');
@@ -12,9 +14,9 @@ const updateSelect = (selectedIndex) => {
 
         if (currentIndex === selectedIndex) {
             console.log(currentIndex);
-            day.classList.add('selected'); // Добавляем класс только для выбранного дня
+            day.classList.add('selected');
         } else {
-            day.classList.remove('selected'); // Убираем класс с остальных
+            day.classList.remove('selected');
         }
     });
 }
@@ -28,31 +30,55 @@ const addEventMobile = (btn) => {
     });
 }
 
+const createBtn = (currentDate) => {
+    const dayBtn = document.createElement('button');
+
+    dayBtn.dataset.index = idFormatter.format(currentDate);
+    dayBtn.classList.add('d-flex', 'align-items-center', 'flex-column', 'p-2');
+
+    const dateText = document.createElement('h4');
+    dateText.textContent = dayMonthFormater.format(currentDate);
+    dayBtn.appendChild(dateText);
+
+    const weekdayText = document.createElement('p');
+    weekdayText.textContent = weekdayFormater.format(currentDate);
+    dayBtn.appendChild(weekdayText);
+    addEventMobile(dayBtn);
+    return dayBtn;
+}
+
 const renderTwoMonth = (startOfDay) => {
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < 70; i++) {
         const currentDate = new Date(startOfDay.getFullYear(), startOfDay.getMonth(), startOfDay.getDate() + i);
-        const dayBtn = document.createElement('button');
-
-        dayBtn.dataset.index = idFormatter.format(currentDate); // Установите index один раз
-        dayBtn.classList.add('d-flex', 'align-items-center', 'flex-column', 'p-2');
-
-        const dateText = document.createElement('h4');
-        dateText.textContent = dayMonthFormater.format(currentDate);
-        dayBtn.appendChild(dateText);
-
-        const weekdayText = document.createElement('p');
-        weekdayText.textContent = weekdayFormater.format(currentDate);
-        dayBtn.appendChild(weekdayText);
-        addEventMobile(dayBtn);
-
+        const dayBtn = createBtn(currentDate);
         calendarMobile.appendChild(dayBtn);
     }
 };
 
 let currentDate = new Date();
-renderTwoMonth(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 30));
+endIndexMobile = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 45)
+startIndexMobile = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 30)
+renderTwoMonth(endIndexMobile);
 
 selectIndex = idFormatter.format(currentDate);
 const todayButtonElement = Array.from(document.querySelectorAll('#calendar-mobile button')).find(btn => btn.dataset.index === selectIndex);
 todayButtonElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
 todayButtonElement.classList.add('selected')
+
+calendarMobile.addEventListener('scroll', () => {
+    if (calendarMobile.scrollLeft + calendarMobile.clientWidth >= calendarMobile.scrollWidth * 0.9) {
+        for (let i = 1; i <= 30; i++) {
+            endIndexMobile = new Date(endIndexMobile.getFullYear(), endIndexMobile.getMonth(), endIndexMobile.getDate()+1);
+            const endBtn = createBtn(endIndexMobile);
+            calendarMobile.appendChild(endBtn);
+        }
+    }
+
+    if (calendarMobile.scrollLeft <= calendarMobile.clientWidth * 0.1) {
+        for (let i = 1; i <= 30; i++) {
+            startIndexMobile = new Date(startIndexMobile.getFullYear(), startIndexMobile.getMonth(), startIndexMobile.getDate()-1);
+            const endBtn = createBtn(startIndexMobile);
+            calendarMobile.prepend(endBtn);
+        }
+    }
+});
